71e3007f5f8f5fabee4e31f7b479ba8068019881
Sandhiyakarthik <120768901+Sandhiyakarthik@users.noreply.github.com>
2023-10-28 21:11:39 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,20 @@
-n=(int(input("enter number of terms")))
-a=0
-b=1
-s=0
+def generate_fibonacci(n):
+    fib_sequence = [0, 1]  # Initialize the sequence with the first two numbers
 
-for x in range(n):
-      print(s,end=" ")
-      s=a+b
-      b=a
-      a=s
-      
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return fib_sequence
+
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    return fib_sequence
+
+# Change the value of 'n' to generate a different number of Fibonacci numbers
+n = 10
+fib_numbers = generate_fibonacci(n)
+print(f"Fibonacci sequence of the first {n} numbers: {fib_numbers}")
