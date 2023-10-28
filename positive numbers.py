2807cc05c0ebf236c335967f03a7bc228b727b93
Sandhiyakarthik <120768901+Sandhiyakarthik@users.noreply.github.com>
2023-10-28 21:16:45 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,19 @@
+# Input list
+list1 = [12, -7, 5, 64, -14]
+
+# Using a list comprehension to filter positive numbers
+positive_numbers = [num for num in list1 if num > 0]
+
+# Printing the positive numbers
+print("Input:", list1)
+print("Output:", positive_numbers)
+
+# Input list
+list2 = [12, 14, -95, 3]
+
+# Using a list comprehension to filter positive numbers
+positive_numbers = [num for num in list2 if num > 0]
+
+# Printing the positive numbers
+print("Input:", list2)
+print("Output:", positive_numbers)
